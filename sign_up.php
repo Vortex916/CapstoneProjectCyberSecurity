3e70fe6bed1d56c7f9f9c9d5156a6bc35848d89c
Vortex916 <82086977+Vortex916@users.noreply.github.com>
2021-04-13 23:21:25 +0200

    ssdgh

--- sign_up.php
+++ sign_up.php
@@ -61,7 +61,9 @@ if(isset($_POST['username'], $_POST['password'], $_POST['passverif'], $_POST['em
 					echo '<script type="text/javascript">alert("No SQL connection error")</script>';
 				}
 				
-				$dn = mysqli_num_rows(mysqli_query($link, 'select id from users where username="'.$username.'"'));
+				$query_result = mysqli_query($link, 'select id from users where username="'.$username.'"')
+				echo '<script type="text/javascript">alert("Finished query")</script>';
+				$dn = mysqli_num_rows($query_result);
 				echo '<script type="text/javascript">alert("Finished mysqli_num_rows()")</script>';
 				
 				if($dn == 0)
