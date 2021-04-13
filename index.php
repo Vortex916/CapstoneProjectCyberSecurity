f40826ae10f6784208cf7acaeb700e4687ce67ee
Vortex916 <82086977+Vortex916@users.noreply.github.com>
2021-04-13 13:04:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -17,19 +17,19 @@ include('config.php');
         <div class="content">
 
 <?php
-//We display a welcome message, if the user is logged, we display it username
+//If the user is logged, we display it username
+if(isset($_SESSION['username'])) 
+{
+	echo ' '.htmlentities($_SESSION['username'], ENT_QUOTES, 'UTF-8');
+	echo '<br>'
+}
 ?>
 
-Hello
-<?php
-if(isset($_SESSION['username'])) {
-	echo ' '.htmlentities($_SESSION['username'], ENT_QUOTES, 'UTF-8');}
-?>
+Welcome on the Cybersecurity Capstone Project (University of Maryland/Coursera) page.
+<br>
+<br>
 
-<br />
-Welcome on our website.<br />
 <?php
-
 //If the user is logged, we display links to edit his infos, to see his pms and to log out
 if (isset($_SESSION['username'])) {
 	echo 'You can <a href="users.php">see the list of users</a>.<br /><br />';
