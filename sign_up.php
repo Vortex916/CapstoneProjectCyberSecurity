e4c84a55d787af1b7407ec69b70dfb6b138fe63d
Vortex916 <82086977+Vortex916@users.noreply.github.com>
2021-04-14 20:58:48 +0200

    Update sign_up.php

--- sign_up.php
+++ sign_up.php
@@ -17,15 +17,15 @@ include('config.php');
 <?php
 
 // see informations.txt
-//$sql_table = "CREATE TABLE users (
-//  id bigint(20) NOT NULL,
-//  username varchar(255) NOT NULL,
-//  password varchar(255) NOT NULL,
-//  email varchar(255) NOT NULL,
-//  avatar text NOT NULL,
-//  signup_date int(10) NOT NULL,
-//  salt varchar(255) NOT NULL
-//)";
+$sql_table = "CREATE TABLE users (
+  id bigint(20) NOT NULL,
+  username varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  avatar text NOT NULL,
+  signup_date int(10) NOT NULL,
+  salt varchar(255) NOT NULL
+)";
 //) ENGINE=MyISAM DEFAULT CHARSET=utf8";
 
 //Check if table users exists in database
