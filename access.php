a8ded55ddbc7c09f2eb2f1d723b2339f62cd657e
Vortex916 <82086977+Vortex916@users.noreply.github.com>
2021-04-17 15:34:23 +0200

    Update access.php

--- access.php
+++ access.php
@@ -39,17 +39,17 @@ else
 		/* bind parameters for markers */
 		if ($stmt->bind_param("s", $username_test))
 		{
-			echo "bind_param successful";
+			echo "bind_param successful<br />";
 		}
 		else
 		{
-			echo "bind_param not successful";
+			echo "bind_param not successful<br />";
 		}
 		$username_test = "Tester4";
 		
 		/* execute query */
 		$stmt->execute();
-		echo "execute successful";
+		echo "execute successful<br />";
 		
 		//We get the password of the user
 		//echo '<script type="text/javascript">alert("prepare")</script>';
@@ -78,9 +78,11 @@ else
   		//$stmt->close();
 
 		$req = mysqli_query($link, 'select password,id,salt from users where username="'.$username.'"');
+		echo "query successful<br />";
 		$dn  = mysqli_fetch_array($req);
-		//echo '<script type=\"text/javascript\">alert("access salt")</script>';
+		echo "fetch array successful<br />";
 		$password = hash("sha512", $dn['salt'].$password); // Hash with the salt to match database.
+		echo "password successful<br />";
 		
 		//We compare the submited password and the real one, and we check if the user exists
 		if ($dn['password'] == $password and mysqli_num_rows($req)>0) 
